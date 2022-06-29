0542f4e4646ed7ba543ab006973fa1756ef58c39
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-06-29 23:08:34 +0530

    understanding dino

--- dino.py
+++ dino.py
@@ -1,5 +1,7 @@
 import turtle
 import time
+import random
+
 
 # GAME VARS
 SCREEN_WIDTH = 800
@@ -11,6 +13,7 @@ ACCELARATION = 0.4
 scr = turtle.Screen()
 scr.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 scr.bgpic('dino_bg.gif')
+scr.title('DINO - CodeWithAaqid')
 scr.tracer(0)
 
 
@@ -30,24 +33,31 @@ dino.dy = 0
 # list of objects
 obstacles = []
 heights = [3,4,5]
-x_spawns = [400, 600, 800]
+x_spawns = [400, 800, 1200]
+
 
 
 for i in range(3):
 	obstacles.append(turtle.Turtle())
+
+
 
 index = 0
 for obstacle in obstacles:
 	obstacle.shape('square')
 	obstacle.color('white')
 	obstacle.penup()
-	obstacle.shapesize(stretch_wid=heights[index], stretch_len=1)
-	obstacle.goto(x_spawns[index], -250)
+	rand_index = random.choice([0,1,2])
+	obstacle.curr_height = heights[rand_index] * 20
+	obstacle.shapesize(stretch_wid=heights[rand_index], stretch_len=1)
+	obstacle.goto(x_spawns[index], -(300 - (obstacle.curr_height//2)))
 	index += 1
 
-
-
-
+# GAME MENU
+menu = turtle.Turtle()
+menu.color('white')
+menu.hideturtle()
+menu.penup()
 
 
 
@@ -56,28 +66,60 @@ def jump():
 	if dino.ycor() == -270: # if dino is on the ground
 		dino.dy = 10
 
+def start_game(x, y):
+	global is_running
+	is_running = True
+
 
 scr.listen()
 scr.onkeypress(jump, 'space')
+scr.onscreenclick(start_game)
 
-
+is_running = False
 while True:
 
 
 	# make dino fall
-	dino.sety(dino.ycor() + dino.dy)
+	if is_running:
+		# get ready
+		dino.showturtle()
+		menu.clear()
 
-	dino.dy -= ACCELARATION
+		# main loop
+		dino.sety(dino.ycor() + dino.dy)
+
+		dino.dy -= ACCELARATION
 
 
-	if dino.ycor() < -270:
-		dino.sety(-270)
-		dino.dy = 0
+		if dino.ycor() < -270:
+			dino.sety(-270)
+			dino.dy = 0
 
 
+		index = 0
+		for obstacle in obstacles:
 
-	for obstacle in obstacles:
-		obstacle.setx(obstacle.xcor() - 3)
+			obstacle.setx(obstacle.xcor() - 3)
+
+			# respawn
+			# just remake the same thing but write it yourself
+			if obstacle.xcor() < -400:
+					rand_index = random.choice([0,1,2])
+					obstacle.curr_height = heights[rand_index] * 20
+					obstacle.shapesize(stretch_wid=heights[rand_index], stretch_len=1)
+					obstacle.goto(800, -(300 - (obstacle.curr_height//2)))
+
+			index += 1
+
+			# check for collision
+			
+
+
+	else:
+		dino.hideturtle()
+		menu.clear()
+		menu.write("Click to Play", align="center", font=('OpenSans', 48, 'bold'))
+
 
 
 	scr.update()
--- multiple.py
+++ multiple.py
@@ -1,17 +1,39 @@
 import turtle
-
+import time
+import random
 
 scr = turtle.Screen()
 scr.setup(width=800, height=600)
 scr.tracer(0)
 
 
-
-
-
-
 balls = []
 for i in range(100):
-	balls.append(turtle.Turtle())
+	ball = turtle.Turtle()
+	ball.shape('circle')
+	ball.penup()
+	ball.dx = random.random() * random.choice([10,-10])
+	ball.dy = random.random() * random.choice([10,-10])
+	balls.append(ball)
+
+while True:
+
+	for ball in balls:
+		ball.setx(ball.xcor() + ball.dx)
+		ball.sety(ball.ycor() + ball.dy)
+
+
+		# top and bottom collision
+		if ball.ycor() > 290 or ball.ycor() < -290:
+			ball.dy = -ball.dy
+
+		# right and left collision
+		if ball.xcor() > 390 or ball.xcor() < -390:
+			ball.dx = -ball.dx
+
+
+
+	scr.update()
+	time.sleep(1/240)
 
 
