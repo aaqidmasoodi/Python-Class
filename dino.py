6c0e7096d7da2c48dbdacbd7bdb606b8f6b28279
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-06-29 09:19:15 +0530

    started dino game

--- dino.py
+++ dino.py
@@ -1,14 +1,16 @@
 import turtle
 import time
 
-
 # GAME VARS
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+ACCELARATION = 0.4
+
 
 # window 
 scr = turtle.Screen()
 scr.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+scr.bgpic('dino_bg.gif')
 scr.tracer(0)
 
 
@@ -17,35 +19,45 @@ scr.tracer(0)
 dino = turtle.Turtle()
 dino.penup()
 dino.shape('square')
+dino.color('white')
 dino.shapesize(stretch_len=2, stretch_wid=2)
-dino.goto(-350, -50)
-dino.x_change = 0
-dino.velocity = 4
+dino.goto(-300, -50)
 dino.dy = 0
 
-def move_dino_right() -> None:
-	dino.x_change = dino.velocity
 
 
-def move_dino_left() -> None:
-	dino.x_change = -dino.velocity
+
+# list of objects
+obstacles = []
+heights = [3,4,5]
+x_spawns = [400, 600, 800]
 
 
-def stop_dino() -> None:
-	dino.x_change = 0
+for i in range(3):
+	obstacles.append(turtle.Turtle())
+
+index = 0
+for obstacle in obstacles:
+	obstacle.shape('square')
+	obstacle.color('white')
+	obstacle.penup()
+	obstacle.shapesize(stretch_wid=heights[index], stretch_len=1)
+	obstacle.goto(x_spawns[index], -250)
+	index += 1
+
+
+
+
+
 
 
 def jump():
-	dino.dy = 10
 
+	if dino.ycor() == -270: # if dino is on the ground
+		dino.dy = 10
 
 
 scr.listen()
-scr.onkeypress(move_dino_right, 'd')
-scr.onkeypress(move_dino_left, 'a')
-scr.onkeyrelease(stop_dino, 'd')
-scr.onkeyrelease(stop_dino, 'a')
-
 scr.onkeypress(jump, 'space')
 
 
@@ -55,7 +67,7 @@ while True:
 	# make dino fall
 	dino.sety(dino.ycor() + dino.dy)
 
-	dino.dy -= 0.4
+	dino.dy -= ACCELARATION
 
 
 	if dino.ycor() < -270:
@@ -64,10 +76,8 @@ while True:
 
 
 
-
-
-	# move the dino along x
-	dino.setx(dino.xcor() + dino.x_change)
+	for obstacle in obstacles:
+		obstacle.setx(obstacle.xcor() - 3)
 
 
 	scr.update()
---
+++ multiple.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+scr = turtle.Screen()
+scr.setup(width=800, height=600)
+scr.tracer(0)
+
+
+
+
+
+
+balls = []
+for i in range(100):
+	balls.append(turtle.Turtle())
+
+
